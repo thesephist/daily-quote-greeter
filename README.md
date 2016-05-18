bbb25ad66dc9118ee053da4b024a017463051119
Linus Lee <linus@thelifelongtraveler.com>
2016-05-18 00:12:42 -0400

    First commit - Basic functionality present

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # daily-quote-greeter
 Uses a Quote of the Day API (http://quotes.rest) to make a new terminal login prompt as the day's quote and refresh the Gnome3 desktop background
+
+The basic program is a NodeJS script to get the necessary JSON data from quotes.rest's API, then:
+- Take the quote title, content, and author and make a greeter message, which is saved as a .txt file
+- Dispatch a shell command to download the quote's corresponding background image (wget) and change gnome3's background using gsettings
+
+I scheduled the script as a cron job to run daily and added a 'cat qotd.txt' line to .zshrc
+
---
+++ quotegetter.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+var request = require('request');
+var fs = require('fs');
+
+var sys = require('sys');
+var exec = require('child_process').exec;
+var child;
+
+// quote getter
+
+request({
+    uri: "http://quotes.rest/qod.json",
+    method: "GET",
+    json: true,
+}, function(err, response, quoteJSON) {
+    quote = quoteJSON.contents.quotes[0];
+
+    line = quote.quote;
+    author = quote.author;
+    title = quote.title;
+    background = quote.background;
+
+    var greetText = "Here's the " + title.toLowerCase() + '\n    \"' + line + '\"\n    ~ ' + author + "\n";
+
+    fs.writeFile("/home/user/.qotd/qotd.txt", greetText, function(err) {
+        if (err) throw err;
+    });
+
+    downloadBackground(background, setBackground);
+
+});
+
+function downloadBackground(picURI, callback) {
+    exec("wget -O /home/user/Downloads/background.jpg " + picURI, function (error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+
+        callback();
+    });
+}
+
+function setBackground() {
+    exec("gsettings set org.gnome.desktop.background picture-uri file:///home/user/Downloads/background.jpg", function (error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+    });
+}
+
